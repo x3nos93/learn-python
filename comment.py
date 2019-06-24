d66bece534805c62d43e587abf24c3b1589de507
William Pritchard <William.Pritchard@propharmagroup.com>
2019-06-24 13:15:25 -0600

    Added comment.py

---
+++ comment.py
@@ -0,0 +1,16 @@
+#this file only contains comments
+
+#a comment can be a single block following the hash
+
+#or, a multiline block
+#containing more
+#than one line
+
+"""
+can also be used to exempt
+multilines of commentary
+but Python will still attempt to read the code,
+discarding once it cannot parse. This is not ideal.
+"""
+
+#use hashes and you will be better off!
