dc265574fabf4a70bffad3c040ccb93b5c9c0bcd
William Pritchard <William.Pritchard@propharmagroup.com>
2019-06-25 13:22:05 -0600

    Initial commit. WP

---
+++ numbers.py
@@ -0,0 +1,25 @@
+x = 1.3
+y = 35E3
+z = 1j
+
+#print("Number Type: " + type(x) + ", " + "Number: " + x)
+print(type(x))
+print(x)
+print(" ")
+print(type(y))
+print(y)
+print(" ")
+print(type(z))
+print(z)
+print(" ")
+
+import random
+print(random.randrange(1,100))
+
+a = int(x)
+b = int(y)
+
+print(type(a))
+print(a)
+print(type(b))
+print(b)
